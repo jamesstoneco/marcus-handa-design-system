398a19a69d53fc3a89938db06ffa155e85d6dd6b
James Stone <james@jamesstone.co>
2017-03-02 15:22:27 -0800

    js scroll interaction

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+// home page hero button scroll interaction
+$(document).ready(function() {
+  $("#js-trigger-work").on('click', function() {
+    $('html, body').animate({
+      scrollTop: $( $("#js-marker-work")).offset().top
+    }, 1200);
+  })
+});
