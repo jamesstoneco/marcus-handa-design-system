671828556edfe894742731277b2adc85826e6877
James Stone <james@jamesstone.co>
2017-04-01 23:09:47 -0700

    burndown tasks

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,4 +10,12 @@ $(document).ready(function() {
 
   $('.testimonial-slider').slick();
 
+  $('.outcome-screenshot').on('click', function() {
+    var $modal = $('#outcome-screenshot-modal');
+    $modal.html('<center><img src="' + $(this).find('img').attr('src') +'" alt="" /></center> <button class="close-button" data-close aria-label="Close modal" type="button"> <span aria-hidden="true">&times;</span> </button> ').foundation('open');
+  });
+  $('.outcome-link').on('click', function() {
+    $('.outcome-screenshot')[0].click();
+  });
+
 });
