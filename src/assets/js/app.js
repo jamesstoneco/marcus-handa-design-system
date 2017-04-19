d2db93965d93bb0024153dddaa513eb755242625
James Stone <james@jamesstone.co>
2017-04-19 12:29:30 -0700

    bugfix: js scroll on homepage

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,7 +2,7 @@ $(document).foundation();
 
 // home page hero button scroll interaction
 $(document).ready(function() {
-  $("#js-trigger-work").on('click', function() {
+  $("#js-trigger-view-work").on('click', function() {
     $('html, body').animate({
       scrollTop: $( $("#js-marker-work")).offset().top
     }, 1200);
